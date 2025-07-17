d957fdf078871f5697043817b5dee4006bd83379
Caleb Munene <CalebMuneneK@users.noreply.github.com>
2025-07-17 21:51:36 +0300

    I created the UI components

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
-// https://vite.dev/config/
-export default defineConfig(() => ({
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
   },
   plugins: [
     react(),
